21a3d4ded68f82b76bfe0a8378e441efd81af3ff
timogutsche <timogutsche.lol@googlemail.com>
2023-05-05 18:27:02 +0200

    yay

--- src/main.c
+++ src/main.c
@@ -11,20 +11,12 @@ int main() {
     
     import_gamesting(bitfields, game_string);
 
-    //TODO: ???
-    if(is_player_white) {
-        printf("Whites board:\n");
-        print_board(bitfields[is_player_white]);
-        printf("Blacks board:\n");
-        print_board(bitfields[!is_player_white]);
-        printf("\n");
-    } else {
-        printf("Blacks board:\n");
-        print_board(bitfields[is_player_white]);
-        printf("Whites board:\n");
-        print_board(bitfields[!is_player_white]);
-        printf("\n");
-    }
+    // print game start
+    printf("%s board:\n", is_player_white ? "Whites" : "Blacks");
+    print_board(bitfields[is_player_white]);
+    printf("%s board:\n", is_player_white ? "Blacks" : "Whites");
+    print_board(bitfields[!is_player_white]);
+    printf("\n");
 
     for(int p = 0; p < 1; p++) // TODO: to be moved into own file
     { 
@@ -68,7 +60,7 @@ int main() {
         }
         */
         field moves[2*counts[0]];
-        int piece_arr[2*counts[0]];
+        int piece_index[2*counts[0]];
         int rating[2*counts[0]];
         int count = 0;
 
@@ -86,78 +78,78 @@ int main() {
             for(int k = 0; k < piece_count; k++)
             {
                 rating[count] = evaluation(bitfields, single_moves[k], legal_moves_piece[i], piece_array[i]);
-                piece_arr[count] = piece_array[i];
+                piece_index[count] = piece_array[i];
                 moves[count] = legal_moves_piece[i];
                 count++;
                 
                 //whats happening here ?
                 rating[count] = 0;
-                piece_arr[count] = 0;
+                piece_index[count] = 0;
                 moves[count] = single_moves[k];
                 count++;
             }
         }
         
         //find first maximum rating
-        int loc = 0;
+        int max_rating_index = 0;
         int total_legal_moves = counts[0];
-        for(int c = 0; c < 2*counts[0]; c++){
-            if(rating[c] == -9999)
+        for(int c = 0; c < 2*counts[0]; c++)
+        {
+            if(rating[c] == -9999.0f) //TODO: define const
                 total_legal_moves--;
-            if(rating[c] > rating[loc])
-                    loc = c;
+
+            if(rating[c] > rating[max_rating_index])
+                max_rating_index = c;
         }
             
-        // make move
-        // move current_piece in piece_board
-        bitfields[is_player_white] ^= moves[loc];
-        bitfields[piece_arr[loc]] ^= moves[loc];
-        for(int i = 0; i < 8; i++)
-            bitfields[i] ^= (moves[loc+1] & bitfields[i]); 
-        bitfields[is_player_white] ^= moves[loc+1];
-        bitfields[piece_arr[loc]] ^= moves[loc+1];
+        // maximum rating move. remove piece on player and piece boards
+        bitfields[is_player_white] ^= moves[max_rating_index];
+        bitfields[piece_index[max_rating_index]] ^= moves[max_rating_index];
 
-        if(piece_arr[loc] == king){
+        //make actual move
+        for(int i = 0; i < 8; i++)
+            bitfields[i] ^= (moves[max_rating_index+1] & bitfields[i]); 
+
+        // TODO: ? pls explain
+        bitfields[is_player_white] ^= moves[max_rating_index+1];
+        bitfields[piece_index[max_rating_index]] ^= moves[max_rating_index+1];
+
+        if(piece_index[max_rating_index] == king)
+        {
             castle_left[is_player_white] = false;
             castle_right[is_player_white] = false;
-        }else if(piece_arr[loc] == rook){
-            if(moves[loc] & h_file)
+        }
+        else if(piece_index[max_rating_index] == rook)
+        {
+            if(moves[max_rating_index] & h_file)
                 castle_right[is_player_white] = false;
-            if(moves[loc] & a_file)
+
+            if(moves[max_rating_index] & a_file)
                 castle_left[is_player_white] = false;
         }
 
+        // print results
         field t = stop_stopwatch(time);
         printf("Of all %d moves %d were legal moves and took %lu us\n", counts[0], total_legal_moves, t);
-        if(is_player_white){
-            printf("Best move for white:\n");
-            print_board(moves[loc]);
-            printf("to:\n");
-            print_board(moves[loc+1]);
-            printf("Whites board:\n");
-            print_board(bitfields[is_player_white]);
-            printf("Blacks board:\n");
-            print_board(bitfields[!is_player_white]);
-            printf("\n");
-        }
-        else{
-            printf("Best move for black:\n");
-            print_board(moves[loc]);
-            printf("to:\n");
-            print_board(moves[loc+1]);
-            printf("Blacks board:\n");
-            print_board(bitfields[is_player_white]);
-            printf("Whites board:\n");
-            print_board(bitfields[!is_player_white]);
-            printf("\n");
-        }
-        //switch sides here
+        
+        printf("Best move for %s:\n", is_player_white ? "white" : "black");
+        print_board(moves[max_rating_index]);
+        printf("to:\n");
+        print_board(moves[max_rating_index+1]);
+        printf("%s board:\n", is_player_white ? "Whites" : "Blacks");
+        print_board(bitfields[is_player_white]);
+        printf("%s board:\n", is_player_white ? "Blacks" : "Whites");
+        print_board(bitfields[!is_player_white]);
+        printf("\n");
+
+        //switch sides
         is_player_white = 1 - is_player_white;
     }
     
     return 0;
 }
 
+// test in main ? :(
 void test_add_to_board_coords(){
     field board = (field) 0;
 
