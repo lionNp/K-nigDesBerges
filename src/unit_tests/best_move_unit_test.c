de9f18ea2cc7ed16a4bd5ba67f2e647a1924b2e9
LionNp <lion.nehring@gmail.com>
2023-05-23 15:23:46 +0200

    was passiert hier?

--- src/unit_tests/best_move_unit_test.c
+++ src/unit_tests/best_move_unit_test.c
@@ -20,60 +20,50 @@ int best_move_for_position(char* fen_string) {
     stopwatch total_time = start_stopwatch();
     field match_duration;
 
-    while(!gameover)
-    //for(int r = 0; r < 1; r++)
-    {
-        //time
-        stopwatch turn_time = start_stopwatch();
-        field t = 0UL;
-        //initilizing moves
-        field moves_from[max_move_count];
-        field moves_to[max_move_count];
-        int piece_idx[max_move_count];
-        //alpha beta initilizing
-        float alpha = losing_move;
-        float beta = winning_move;
-        bool max_player = is_player_white;
-        //get moves and set rating
-        int move_count = generate_moves(moves_from, moves_to, piece_idx);
+    
+    //time
+    stopwatch turn_time = start_stopwatch();
+    field t = 0UL;
+    //initilizing moves
+    field moves_from[max_move_count];
+    field moves_to[max_move_count];
+    int piece_idx[max_move_count];
+    //alpha beta initilizing
+    float alpha = losing_move;
+    float beta = winning_move;
+    bool max_player = is_player_white;
+    //get moves and set rating
+    int move_count = generate_moves(moves_from, moves_to, piece_idx);
 
-        float rating[move_count];
+    float rating[move_count];
 
-        if(move_count == 0){
-            printf("No more moves\n");
-            gameover = true;
-            break;
+    //shouldnt we increse depth in increments of 2, starting at 1? so as to not get half-false results
+    for(int depth = 0; t < 1000000; depth++){ //t < 1000
+        for(int i = 0; i < move_count; i++){    // <- t < 1 000 000
+            field captured[8] = {0UL};
+            make_move(piece_idx[i], moves_from[i], moves_to[i], captured);
+            is_player_white = !is_player_white;
+            rating[i] = alphabeta(depth, alpha, beta, max_player);
+            is_player_white = !is_player_white;
+            unmake_move(piece_idx[i], moves_from[i], moves_to[i], captured);
+            // <- put stopwatch here?
         }
-
-        //shouldnt we increse depth in increments of 2, starting at 1? so as to not get half-false results
-        for(int depth = 0; t < 1000000; depth++){ //t < 1000
-            for(int i = 0; i < move_count; i++){    // <- t < 1 000 000
-                field captured[8] = {0UL};
-                make_move(piece_idx[i], moves_from[i], moves_to[i], captured);
-                is_player_white = !is_player_white;
-                rating[i] = alphabeta(depth, alpha, beta, max_player);
-                is_player_white = !is_player_white;
-                unmake_move(piece_idx[i], moves_from[i], moves_to[i], captured);
-                // <- put stopwatch here?
-            }
-            t = stop_stopwatch(turn_time);
-            printf("time: %ldμs at depth: %d\n", t, depth);
-        }
-
-        int idx = max_rating(rating, move_count);
-
-        //best move in idx
-        printf("next best move:\n");
-        char from[3];
-        print_position_as_spoken(moves_from[idx], from);
-        printf("from: %s\n", from);
-        //print_board(moves_from[idx]);
-        char to[3];
-        print_position_as_spoken(moves_to[idx], from);
-        printf("to: %s\n", to);
-        //print_board(moves_to[idx]);
-
+        t = stop_stopwatch(turn_time);
+        printf("time: %ldμs at depth: %d\n", t, depth);
     }
+
+    int idx = max_rating(rating, move_count);
+
+    //best move in idx
+    printf("next best move:\n");
+    char from[3];
+    print_position_as_spoken(moves_from[idx], from);
+    printf("from: %s\n", from);
+    //print_board(moves_from[idx]);
+    char to[3];
+    print_position_as_spoken(moves_to[idx], from);
+    printf("to: %s\n", to);
+    //print_board(moves_to[idx]);
 
     return 0;
 }
