5b5e6ab04ddc671149e88b5399e90dad08f02c39
BryanChua1 <bryan.c@hotmail.de>
2023-06-19 11:11:28 +0200

    Update alphabeta.h

--- src/header/alphabeta.h
+++ src/header/alphabeta.h
@@ -7,4 +7,8 @@ float alphabeta(int depth, float alpha, float beta, bool max_player);
 float alphabeta_without_tt(int depth, float alpha, float beta, bool max_player);
 float pvs(int depth, float alpha, float beta, bool max_player);
 float pvs_no_tt(int depth, float alpha, float beta, bool max_player);
+float abqs(int depth, float alpha, float beta, bool max_player);
+float qs(int depth, float alpha, float beta, bool max_player);
+//MostValuableVictim-LeastValuableAttacker-Sorting--Bubblesort
+void bsMVV_LVA(field moves_from[], field moves_to[], int piece_idx[], int movecount);
 #endif
