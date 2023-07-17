2ad99b2b910a7bc7b74ee55174e46880c06beb81
LionNp <lion.nehring@gmail.com>
2023-07-17 17:51:26 +0200

    hi

--- train_weights.sh
+++ train_weights.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc "weights_training.c" -o "build/main" -Isrc/header -lm -lpthread -fgnu89-inline
+gcc "src/test/weights_training.c" -o "build/main" -Isrc/header -lm -lpthread -fgnu89-inline
 "./build/main"
 exit 0
 
